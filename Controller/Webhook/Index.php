81b734277c4304627089d00966ecf8085c545422
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2016-03-02 13:10:42 -0600

    URL webhook funcional [1.9.7-edv]

--- Controller/Webhook/Index.php
+++ Controller/Webhook/Index.php
@@ -3,13 +3,24 @@ namespace Compropago\Magento2\Controller\Webhook;
 
 class Index extends \Magento\Framework\App\Action\Action {
 
-    /**
-	 * @override
-	 * @see \Magento\Framework\App\Action\Action::execute()
-	 * @return \Magento\Framework\Controller\Result\Redirect
-	 */
-	public function execute() {
-		return $this->resultRedirectFactory->create()->setPath('catalog/product/');
-	}
+    /** @var \Magento\Framework\Controller\Result\JsonFactory */
+    protected $resultFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Controller\ResultFactory $resultFactory
+    ) {
+        parent::__construct($context);
+        $this->resultFactory = $resultFactory;
+    }
+
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Raw $result */
+        $result = $this->resultFactory->create('raw');
+        /** You may introduce your own constants for this custom REST API */
+        $result->setContents('hola mundo');
+        return $result;
+    }
 
 }
